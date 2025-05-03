535dba16851cffecf9f3cb37e240eeaaa3bf83d5
Aleksandr Mironenko <a.mironenko@initi.ru>
2025-05-03 18:07:57 +0300

    added module for setup

---
+++ setup/loader_methods.js
@@ -0,0 +1,19 @@
+const { loader, Loader } = require("@loader");
+
+global.loadBulk = function (dependencies, callback, error) {
+    return new Promise((resolve, reject) => {
+        const success = () => {
+            if (callback instanceof Function) {
+                callback();
+            }
+            resolve();
+        };
+        const fail = () => {
+            if (error instanceof Function) {
+                error();
+            }
+            reject(new Error("Failed to load dependencies", dependencies));
+        };
+        loader.loadBulk(dependencies, success, fail);
+    });
+};

321deeb1c1a37be11cfc24b7b89d1de6ec3f0fbc
Aleksandr Mironenko <a.mironenko@initi.ru>
2025-05-10 12:05:33 +0300

    - removed loading global deps from package. Because we have to decide outside what we need preload as system global deps - created loadGlobalDeps for vitest.setup.js file (in setup/ dir)

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,6 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import { init } from "initi-loader";
+import loadGlobalDeps from "./setup/loadGlobalDeps.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -27,4 +28,5 @@ const options = {
 
 if (globalThis.v2 === undefined) {
     await init(options);
+    await loadGlobalDeps();
 }
